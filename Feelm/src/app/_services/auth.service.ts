538f1741a97d4898b438bec835460c0529a962a2
Sergio Leopoldo Benítez Delgado <alu0101244440@ull.edu.es>
2022-01-09 13:38:42 +0000

    Permite autentificacion y registro con MongoDB local

--- Feelm/src/app/_services/auth.service.ts
+++ Feelm/src/app/_services/auth.service.ts
@@ -1,20 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+
   // URL de la api (backend)
   private URL = "http://localhost:3000/api"
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   // Esté método hará la petición a nuestro servidor para añadir un nuevo usuario a la BD
   register(user) {
     return this.http.post<any>(this.URL + '/signup', user);
+  }
+
+  login(user) {
+    return this.http.post<any>(this.URL + '/signin', user);
+  }
+
+  loggedIn(): Boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/navigate']);
   }
 }
